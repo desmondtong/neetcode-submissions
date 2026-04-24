55f3b4f4fbc6e7c6a0d8550a011ebc280168f84b
Desmond Tong <107420497+desmondtong@users.noreply.github.com>
2026-04-24 23:56:39 +0800

    Add: trapping-rain-water - submission-3

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-3.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+        if(height.length == 0) return 0
+
+        let l = 0;
+        let r = height.length - 1;
+        let lMax = height[l];
+        let rMax = height[r];
+
+        let waterTrapped = 0;
+        while (l < r) {
+            if (lMax < rMax) {
+                l++;
+                lMax = Math.max(lMax, height[l]);
+                waterTrapped += lMax - height[l];
+            } else {
+                r--;
+                rMax = Math.max(rMax, height[r]);
+                waterTrapped += rMax - height[r];
+            }
+        }
+
+        return waterTrapped;
+    }
+}
