069787afa7bca9f8b52f2dab2047a6065418b570
Desmond Tong <107420497+desmondtong@users.noreply.github.com>
2026-04-28 00:17:14 +0800

    Add: sliding-window-maximum - submission-5

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-5.js
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    maxSlidingWindow(nums, k) {
+        if (k == 1) return nums;
+
+        // initialise maxHeap
+        const maxHeap = new PriorityQueue((a, b) =>
+            b.num != a.num ? b.num - a.num : a.idx - b.idx,
+        );
+        for (let i = 0; i < k; i++) {
+            maxHeap.enqueue({ num: nums[i], idx: i });
+        }
+
+        // as the sliding window proceeds,
+        let res = [];
+        for (let i = 0, j = i + k - 1; j < nums.length; i++, j++) {
+            // push max into res
+            let { num, idx } = maxHeap.front();
+            // console.log(maxHeap.front(), i, j);
+            res.push(num);
+
+            // update heap
+            if (j != nums.length - 1) {
+                // check if top still valid
+                while (idx <= i) {
+                    maxHeap.dequeue();
+                    idx = maxHeap.front().idx;
+                }
+
+                // add next element
+                maxHeap.enqueue({ num: nums[j + 1], idx: j + 1 });
+            }
+        }
+
+        return res;
+    }
+}
