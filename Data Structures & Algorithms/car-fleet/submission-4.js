4052325fe6c3f5e065c84184f92ba1332e92f44a
Desmond Tong <107420497+desmondtong@users.noreply.github.com>
2026-04-30 18:26:00 +0800

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target, position, speed) {
+        // distance / speed = timeSpent
+
+        let arr = [];
+        for (let i = 0; i < position.length; i++) {
+            const timeSpent = (target - position[i]) / speed[i];
+            arr.push([position[i], speed[i], timeSpent]);
+        }
+        arr = arr.sort((a, b) => b[0] - a[0]);
+
+        let stack = [];
+        for (let i = 0; i < arr.length; i++) {
+            if (stack.length == 0) {
+                stack.push(arr[i][2]);
+            } else {
+                const prevSpeed = stack[stack.length - 1];
+                if (arr[i][2] > prevSpeed) {
+                    stack.push(arr[i][2]);
+                }
+            }
+        }
+
+        return stack.length;
+    }
+}
