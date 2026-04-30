e77d63a616bf7cf3da3861fd00782ab4f951efee
Desmond Tong <107420497+desmondtong@users.noreply.github.com>
2026-04-30 17:14:20 +0800

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        let res = new Array(temperatures.length).fill(0);
+        let stack = []; // idx
+
+        for (let i = 0; i < temperatures.length; i++) {
+            const curTemp = temperatures[i];
+            if (stack.length > 0) {
+                let popped = stack[stack.length - 1];
+                // console.log(`${i}outWhile:`, popped);
+
+                while (curTemp > temperatures[popped] && stack.length > 0) {
+                    stack.pop();
+                    // console.log(`${i}inWhile:`, popped);
+                    res[popped] = i - popped;
+
+                    if (stack.length > 0) popped = stack[stack.length - 1];
+                }
+            }
+
+            stack.push(i);
+        }
+
+        return res;
+    }
+}
