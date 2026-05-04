4a88c345569826b3c8e3b2b0edde52aff7649320
Desmond Tong <107420497+desmondtong@users.noreply.github.com>
2026-05-04 18:43:13 +0800

    Add: find-target-in-rotated-sorted-array - submission-2

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-2.js
@@ -0,0 +1,54 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        // find min / cut point
+        let l = 0;
+        let r = nums.length - 1;
+
+        while (l < r) {
+            let m = Math.floor((l + r) / 2);
+            // console.log(l, m, r);
+
+            if (nums[l] <= nums[m] && nums[m] < nums[r]) break;
+            if (nums[l] <= nums[m]) {
+                // min on right portion
+                l = m + 1;
+            } else if (nums[m] < nums[r]) {
+                // min on left portion
+                r = m;
+            }
+        }
+
+        if (
+            (nums[l] < target && target < nums[nums.length - 1]) ||
+            nums[l] == target ||
+            nums[nums.length - 1] == target
+        ) {
+            return bSearch(l, nums.length - 1, nums, target);
+        } else {
+            return bSearch(0, l - 1, nums, target);
+        }
+    }
+}
+
+function bSearch(left, right, nums, target) {
+    let l = left;
+    let r = right;
+
+    while (l <= r) {
+        let m = Math.floor((l + r) / 2);
+
+        if (nums[m] === target) return m;
+        if (nums[m] < target) {
+            l = m + 1;
+        } else {
+            r = m - 1;
+        }
+    }
+
+    return -1;
+}
