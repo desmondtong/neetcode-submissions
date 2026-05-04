db73168621a59b670372b9ae3894a80d5f5f9136
Desmond Tong <107420497+desmondtong@users.noreply.github.com>
2026-05-04 16:27:54 +0800

    Add: eating-bananas - submission-6

---
+++ Data Structures & Algorithms/eating-bananas/submission-6.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+        let l = 0;
+        let r = Math.max(...piles);
+        let k = Infinity;
+        while (l <= r) {
+            let m = Math.floor((l + r) / 2);
+
+            // const isSuf = isSufficient(h, m + 1, piles);
+            let numberOfHours = 0;
+            for (let idx = 0; idx < piles.length; idx++) {
+                numberOfHours += Math.ceil(piles[idx] / (m + 1));
+            }
+
+            if (numberOfHours <= h) {
+                r = m - 1;
+                k = Math.min(k, m + 1);
+            } else {
+                l = m + 1;
+            }
+        }
+
+        return k;
+    }
+}
+
+function isSufficient(h, rate, piles) {
+    let numberOfHours = 0;
+    for (let idx = 0; idx < piles.length; idx++) {
+        numberOfHours += Math.ceil(piles[idx] / rate);
+    }
+
+    return numberOfHours <= h ? true : false;
+}
