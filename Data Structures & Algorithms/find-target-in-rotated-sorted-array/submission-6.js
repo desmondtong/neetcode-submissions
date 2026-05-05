1e874e34dfa15428ae37af12cba393660a67f17f
Desmond Tong <107420497+desmondtong@users.noreply.github.com>
2026-05-05 11:46:02 +0800

    Add: find-target-in-rotated-sorted-array - submission-6

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-6.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        // find min / cut point
+        let l = 0;
+        let r = nums.length - 1;
+
+        while (l <= r) {
+            let m = Math.floor((l + r) / 2);
+            // console.log(l, m, r);
+
+            if (nums[m] == target) return m;
+            if (nums[l] == target) return l;
+            if (nums[r] == target) return r;
+
+            if (nums[l] <= nums[m]) {
+                // left portion sorted
+                if (target < nums[m] && target > nums[l]) {
+                    r = m;
+                } else {
+                    l = m + 1;
+                }
+            } else if (nums[m] < nums[r]) {
+                // right portion sorted
+                if (target > nums[m] && target < nums[r]) {
+                    l = m + 1;
+                } else {
+                    r = m;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
